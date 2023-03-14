c150a6e534e6bcf09c4a4362995f2c32ddbc9a55
Pippa101 <94748151+Pippa101@users.noreply.github.com>
2023-03-14 16:06:55 +0000

    making comments component before seperating out api-logic

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react";
 
 const [isLoading, setIsLoading] = useState(true);
+const [articleComments, setArticleComments] = useState([]);
+
+const FetchArticleComments = (article_id) => {
+  return fetch(
+    `https://n3ws.onrender.com/api/articles/${article_id}/comments`
+  ).then((response) => response.json());
+};
 
 const Comments = () => {
   useEffect(() => {
-    fetch(`https://n3ws.onrender.com/api/articles/${article_id}/comments`)
-      .then((response) => response.json())
-      .then((body) => {
-        setArticleComments(body);
-        setIsLoading(false);
-      });
-  }, []);
+    setIsLoading(true);
+    FetchArticleComments(article_id).then((body) => {
+      setArticleComments(body);
+      setIsLoading(false);
+    });
+  }, [article_id]);
+
+  return (
+    <section>
+      <h3>Leave A Comment</h3>
+      {articleComments.map((comment) => {
+        <article>
+          <p>{comment.author}</p>
+          <p>{comment.created_at}</p>
+          <p>{comment.body}</p>
+          <p>votes:{comment.votes}</p>
+        </article>;
+      })}
+    </section>
+  );
 };
 
 export default Comments;
